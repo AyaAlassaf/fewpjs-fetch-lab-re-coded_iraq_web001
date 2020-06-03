00405e3d1420290db7cfcd14ebd1ebf6b22d53a3
Aya Alassaf <ayafaisal242@yahoo.com>
2020-06-03 13:16:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function fetchBooks() {
   return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json =>
-  console.log(json));
-  renderBooks;
+  renderBooks(json));
+  ;
 
 }
 
